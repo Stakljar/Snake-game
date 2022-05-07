bc1d2cad8499851307b3babc6a46277249575b07
Austrax <Austrax@10.4.4.125>
2022-05-07 18:42:02 +0200

    Commit from Eclipse IDE

---
+++ Snake game/src/hr/ferit/snake/Direction.java
@@ -0,0 +1,14 @@
+package hr.ferit.snake;
+
+import java.util.Random;
+
+public enum Direction {
+	
+	NORTH, WEST, SOUTH, EAST;
+	
+	public Direction getRandomDirection() {
+		
+		int randomDirection = new Random().nextInt(Direction.values().length);
+	    return Direction.values()[randomDirection];
+	}
+}
---
+++ Snake game/src/hr/ferit/snake/GameInitiator.java
@@ -0,0 +1,10 @@
+package hr.ferit.snake;
+
+@SuppressWarnings("unused")
+public class GameInitiator {
+
+	public static void main(String[] args) {
+		
+		JSnake snake = new JSnake();
+	}
+}
